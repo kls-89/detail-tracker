a057df53b25ae984a40dc1c4f2f81103b001faae
Keenan Leonard-Solis <k.leonardsolis@gmail.com>
2020-06-17 12:15:20 -0400

    Create connection to MongoDB Atlas

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(bodyParser.json());
@@ -9,4 +10,13 @@ app.get("/", (req, res) => {
   res.send("connected");
 });
 
-app.listen(process.env.PORT, () => console.log("server running on port", process.env.PORT));
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB Atlas');
+    app.listen(process.env.PORT, () => console.log("server running on port", process.env.PORT));
+  })
+  .catch(error => {
+    console.error("Error connecting to MongoDB Atlas", error);
+  });
+
+
