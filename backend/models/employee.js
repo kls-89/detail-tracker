f3e9c7c8eef034b514e94ce524b7ccccbee05dea
Keenan Solis <k.leonardsolis@gmail.com>
2020-06-30 09:06:33 -0400

    Update employee email and phone number with regex match.

--- backend/models/employee.js
+++ backend/models/employee.js
@@ -14,11 +14,14 @@ const employeeSchema = new mongoose.Schema({
   },
   phoneNumber: {
     type: String,
-    required: true
+    required: true,
+    match: /^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/
   },
   emailAddress: {
     type: String,
-    required: true
+    required: true,
+    unique: true,
+    match: /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/
   },
   passwordHash: {
     type: String,
